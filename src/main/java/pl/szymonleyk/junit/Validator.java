7efea5ec31cf762e01fbb96faffbe11fb87affdc
Maria Andrejczuk <Maria.Andrejczuk@gmail.com>
2021-03-19 07:57:03 +0100

    #1 implementacja do testu 2

--- src/main/java/pl/szymonleyk/junit/Validator.java
+++ src/main/java/pl/szymonleyk/junit/Validator.java
@@ -3,7 +3,7 @@ package pl.szymonleyk.junit;
 public final class Validator {     //final - bo nie chcemy jej przeciażać
 
     public static boolean peselValidation(String pesel) {
-        return false;
+        return pesel.length() == 11;
     }
 }
 
