9d017e50c7c177190c5aa4d0a86db985a7d10349
Maria Andrejczuk <Maria.Andrejczuk@gmail.com>
2021-03-19 08:12:18 +0100

    #1 validacja dnia

--- src/main/java/pl/szymonleyk/junit/Validator.java
+++ src/main/java/pl/szymonleyk/junit/Validator.java
@@ -3,7 +3,12 @@ package pl.szymonleyk.junit;
 public final class Validator {     //final - bo nie chcemy jej przeciażać
 
     public static boolean peselValidation(String pesel) {
-        return pesel.length() == 11;
+        return pesel.length() == 11 && validDay(pesel.substring(4,6));
+    }
+
+    private static boolean validDay(String day) {
+        int dayNumber = Integer.parseInt(day);
+        return dayNumber >= 1 && dayNumber <=31;
     }
 }
 
