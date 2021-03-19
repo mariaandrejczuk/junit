6134f2d6c2086780758e500dfdcf71d909bde423
Maria Andrejczuk <Maria.Andrejczuk@gmail.com>
2021-03-19 07:52:27 +0100

    #1 dodanie testu sprawdzającego długość 11, poprawny

---
+++ src/main/java/pl/szymonleyk/junit/Validator.java
@@ -0,0 +1,9 @@
+package pl.szymonleyk.junit;
+
+public final class Validator {     //final - bo nie chcemy jej przeciażać
+
+    public static boolean peselValidation(String pesel) {
+        return false;
+    }
+}
+
--- src/test/java/pl/szymonleyk/junit/ValidatorTest.java
+++ src/test/java/pl/szymonleyk/junit/ValidatorTest.java
@@ -3,6 +3,7 @@ package pl.szymonleyk.junit;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertFalse; //import statyczny, importuję statyczną metodę i mogę do niej się odwołać bez podawania klasy, w której się znajduje
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ValidatorTest {
     @Test
@@ -10,9 +11,18 @@ public class ValidatorTest {
         //given
         String pesel = "1555";
         //when
-        boolean isValid = Validator.pedelValidation(pesel);
+        boolean isValid = Validator.peselValidation(pesel);
         //then
         assertFalse(isValid);
+    }
+    @Test
+    public void isValidIfLength11(){
+        //given
+        String pesel = "11115555555";
+        //when
+        boolean isValid = Validator.peselValidation(pesel);
+        //then
+        assertTrue(isValid);
     }
 }
 
