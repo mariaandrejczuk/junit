6137f8f32be4a5f9ca6737a7de85f283c9c446d5
Maria Andrejczuk <Maria.Andrejczuk@gmail.com>
2021-03-24 07:58:38 +0100

    #3 implementacja testu sprawdzajacego 3-cyfrową liczbę

---
+++ src/test/java/pl/szymonleyk/junit/Validator3Test.java
@@ -0,0 +1,27 @@
+package pl.szymonleyk.junit;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class Validator3Test {
+    @Test
+    public void isValidIfNumberIs3Digit(){
+        //given
+        String threeDigitNumber = "125";
+        //when
+        boolean isValid = Validator3.digitValidation(threeDigitNumber);
+        //then
+        assertTrue(isValid);
+    }
+    @Test
+    public void isNotValidIfNumberIs4Digit(){
+        //given
+        String threeDigitNumber = "1225";
+        //when
+        boolean isValid = Validator3.digitValidation(threeDigitNumber);
+        //then
+        assertFalse(isValid);
+    }
+}
