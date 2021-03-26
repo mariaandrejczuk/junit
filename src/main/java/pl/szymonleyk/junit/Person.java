74ebfcd098dca7ed756f369addca94540b3acafb
Maria Andrejczuk <Maria.Andrejczuk@gmail.com>
2021-03-26 07:49:28 +0100

    #12 test dla metody wyciągającej rok z peselu

---
+++ src/main/java/pl/szymonleyk/junit/Person.java
@@ -0,0 +1,11 @@
+package pl.szymonleyk.junit;
+
+public class Person {
+    private String name;
+    private String pesel;
+
+    public Person(String name, String pesel) {
+        this.name = name;
+        this.pesel = pesel;
+    }
+}
---
+++ src/test/java/pl/szymonleyk/junit/PersonTest.java
@@ -0,0 +1,20 @@
+package pl.szymonleyk.junit;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+public class PersonTest {
+    @ParameterizedTest
+    @CsvSource({"Jurek, 99011202235, 99", "Kasia, 88021114156, 88"})
+    public void isCorrectYear(String name, String pesel, String year){
+        //given
+        Person person = new Person(name, pesel);
+        //when
+        String yearResult = person.getNameFromPesel();
+        //then
+        assertTrue(year, yearResult);
+    }
+}
